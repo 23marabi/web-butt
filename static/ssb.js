8d7ea49b6dbb4b2bf27c9fe52ee8287bf3642688
Erin Nova <contact@the-system.eu.org>
2022-06-01 16:30:40 -0400

    Add favicon, restructure JS

--- static/ssb.js
+++ static/ssb.js
@@ -8,42 +8,13 @@ function whoami() {
 	})
 }
 
-whoami()
+function feed() {
+	$.get("/api/posts", function(response){
+		response.posts.forEach(createPost)
+	})
+}
 
-window.addEventListener( "load", function () {
-	function sendData() {
-		const XHR = new XMLHttpRequest();
-
-    	// Bind the FormData object and the form element
-    	const FD = new FormData( form );
-
-	    // Define what happens on successful data submission
-    	XHR.addEventListener( "load", function(event) {
-      		alert( event.target.responseText );
-    	} );
-
-    	// Define what happens in case of error
-    	XHR.addEventListener( "error", function( event ) {
-      		alert( 'Oops! Something went wrong.' );
-    	} );
-
-    	// Set up our request
-    	XHR.open( "POST", "/api/update" );
-
-    	// The data sent is what the user provided in the form
-    	XHR.send( FD );
-  	}
-
-	// Access the form element...
-	const form = document.getElementById( "profileForm" );
-
-	// ...and take over its submit event.
-	form.addEventListener( "submit", function ( event ) {
-    	event.preventDefault();
-
-    	sendData();
-		whoami();
-	} );
-} );
-
-
+function createPost(value, index, array) {
+	console.log(value)
+	$("#messages").append('<h3>'+value.author+'</h3><p><i>'+value.timestamp+'</p></i>')
+}
